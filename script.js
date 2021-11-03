a95f58d41e178e8ab289bd5882a4a4bd3eb5dabb
ncp9988@gmail.com <v.i.c@Chaus-MacBook-Pro.local>
2021-11-03 13:31:42 -0500

    Validations for length and selection complete

---
+++ script.js
@@ -0,0 +1,79 @@
+// Assignment code here
+
+
+// Get references to the #generate element
+var generateBtn = document.querySelector("#generate");
+
+// Write password to the #password input
+function writePassword() {
+  var password = generatePassword();
+  var passwordText = document.querySelector("#password");
+  // var character = lowercase + uppercase + numeric +special 
+  // let promtLength = i;
+  // for  ( var i = 8; i < 129; i++) {
+  //   password = Math.floor((Math.random()*i.character);
+  // }
+
+  passwordText.value = password;
+
+}
+
+
+// Add event listener to generate button
+generateBtn.addEventListener("click", function () {
+  writePassword();
+});
+
+function generatePassword() {
+
+  //prompt length of password
+  var promptLength = parseInt(window.prompt('Please choose the lenght of password, need to be at least 8 characters and no more than 128 characters'));
+  if (promptLength < 8 || promptLength > 128 || isNaN(promptLength)){
+    return "Invalid length...please enter a number between 8 and 128"
+  }
+
+
+  //ask for character types
+  var useChoiceCharacters = ""
+
+  //Lowercase
+  var promptLower = window.confirm('Would you like to include lowercase letters? Click "OK" for yes');
+  if (promptLower===true) {
+    //var confirmLower = window.confirm("You chose to include lowercase letters.");
+    useChoiceCharacters += "qwertyuiopasdfghjklzxcvbnm";
+  }
+
+  
+  //Uppercase
+  var promptUpper = window.confirm('Would you like to include uppercase letters? Click "OK" for yes');
+  if (promptUpper === true) {
+    
+   useChoiceCharacters += "QWERTYUIOPASDFGHJKLZXCVBNM";
+  }
+
+ 
+  //Numeric
+  var promptNumeric = window.confirm('Would you like to include numeric characters? Click "OK" for yes');
+  if (promptNumeric === true) {
+  
+    useChoiceCharacters += "1234567890";
+  }
+
+  
+
+  //Special Character
+  var promptSpecial = window.confirm('Would you like to include special characters? Click "OK" for yes');
+  if (promptSpecial === true) {
+    
+    useChoiceCharacters += "`~!@#$%^&*()-_=+[]\{}|;:',./<>?";
+  }
+  
+  if ( useChoiceCharacters.length == 0) {
+    return "Please choose at least one option."
+  }
+
+
+}
+
+
+
